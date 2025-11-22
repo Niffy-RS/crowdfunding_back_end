c3140e04f556672ef2eeef423603e34a437f5829
jennifer <jennifer_rs16@hotmail.com>
2025-11-22 12:10:50 +1000

    implemented authentication tokens

--- crowdfunding/fundraisers/serializers.py
+++ crowdfunding/fundraisers/serializers.py
@@ -8,6 +8,7 @@ class PledgeSerializer(serializers.ModelSerializer):
 
 class FundraiserSerializer(serializers.ModelSerializer):
   owner = serializers.ReadOnlyField(source='owner.id')
+  
   class Meta:
       model = apps.get_model('fundraisers.Fundraiser')
       fields = '__all__'
