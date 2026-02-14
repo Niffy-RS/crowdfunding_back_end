9b1395dab93253e8a20cdfc22f6ece33cc724885
jennifer <jennifer_rs16@hotmail.com>
2026-02-14 22:08:56 +1000

    bug fixes

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import CustomUser
-from crowdfunding.serializers import FundraiserSerializer, PledgeSerializer
+from fundraisers.serializers import FundraiserSerializer, PledgeSerializer
 
 class CustomUserSerializer(serializers.ModelSerializer):
     fundraisers = FundraiserSerializer(many=True, read_only=True)
