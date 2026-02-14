fec26a65a0ad98b25bf493f69d81fe94262c637f
jennifer <jennifer_rs16@hotmail.com>
2026-02-14 20:22:10 +1000

    bug fixes

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -4,7 +4,7 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = '__all__'
+        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
